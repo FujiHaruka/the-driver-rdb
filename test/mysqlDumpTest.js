bbd55b477183831c278c46c54296dc23e8d2d28d
okunishinishi <okunishinishi@gmail.com>
2018-11-12 20:38:16 +0900

    Version incremented to 2.0.5

---
+++ test/mysqlDumpTest.js
@@ -0,0 +1,22 @@
+/**
+ * Test for mysqlDump.
+ * Runs with mocha.
+ */
+'use strict'
+
+const mysqlDump = require('../lib/dumps/mysqlDump')
+const { ok, equal } = require('assert')
+
+describe('mysql-dump', () => {
+  before(() => {
+  })
+
+  after(() => {
+  })
+
+  it('Do test', () => {
+
+  })
+})
+
+/* global describe, before, after, it */
